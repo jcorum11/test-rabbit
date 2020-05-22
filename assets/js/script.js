d4f0db68dea222c1768f8fdea8a8d33d44df1a70
Jacob Corum <cor15031@byui.edu>
2020-05-22 09:00:32 -0600

    wrote test array for questions

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var testArr = [
+    {q: "Question 1", a1: "Answer 1", a1: "Answer 2", a1: "Answer 3", a1: "Answer 4"}, 
+    {q: "Question 2", a1: "Answer 1", a1: "Answer 2", a1: "Answer 3", a1: "Answer 4"}, 
+    {q: "Question 3", a1: "Answer 1", a1: "Answer 2", a1: "Answer 3", a1: "Answer 4"}, 
+    {q: "Question 4", a1: "Answer 1", a1: "Answer 2", a1: "Answer 3", a1: "Answer 4"}
+]
+
+
+
