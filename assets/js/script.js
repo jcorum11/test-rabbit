19a65d37cf9a30e5ab93165494eb6a661279f8e0
Jacob Corum <cor15031@byui.edu>
2020-05-24 13:32:54 -0600

    timer adds and subtracts on switch page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,82 @@
 var testArr = [
-    {q: "Question 1", 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}, 
-    {q: "Question 2", 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}, 
-    {q: "Question 3", 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}, 
-    {q: "Question 4", 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}
+    {q: "Question 1", a: 0, 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}, 
+    {q: "Question 2", a: 1, 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}, 
+    {q: "Question 3", a: 2, 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}, 
+    {q: "Question 4", a: 3, 0: "Answer 1", 1: "Answer 2", 2: "Answer 3", 3: "Answer 4"}
 ]
+var id = 0
 var timeLeft = 25;
+var pageContentEl = document.querySelector("#page-content");
 var mainBoxTopEl = document.querySelector(".main-box.top");
 var mainBoxCenterEl = document.querySelector(".main-box.center");
 var mainBoxBottomEl = document.querySelector(".main-box.bottom");
-var pageContentEl = document.querySelector("#page-content");
+var questionEl = document.querySelector("#question")
 var timerEl = document.querySelector("#timer");
-var newMainBoxTopEl = document.createElement("div");
-var newMainBoxCenterEl = document.createElement("div");
-var newMainBoxBottomEl = document.createElement("div");
 
-// timer
-var timerHandler = function(event) {
-    var timerInterval = setInterval(function() {
-        timeLeft--;
-        timerEl.textContent = timeLeft;
-        if (timeLeft === 0) {
-            clearInterval(timerInterval);
-        }
-    }, 1000);
-};
 
-var buttonLoop = function() {
+
+var contentHandler = function() {
+    
+    clearDivContent();
+    
+    // change header
+    var questionObj = testArr[id];
+    var questionStr = questionObj.q
+    questionEl.textContent = questionStr;
+    mainBoxTopEl.appendChild(questionEl);
+
     // add questions to center as buttons. add styles to buttons and divs. 
     for (var i = 0; i <= 3; i++) {
-        if (!i) {
-            // add "Question 1" to top
-            var question = testArr[i]
-            var newH1El = document.createElement("h1");
-            newH1El.textContent = question.q;
-            newMainBoxTopEl.appendChild(newH1El);
-        }
         var answerButtonEl = document.createElement("button");
-        var buttonContainerEl = document.createElement("div");
-        answerButtonEl.textContent = testArr[0][i];
+        answerButtonEl.textContent = testArr[id][i];
         answerButtonEl.className = "btn answer";
-        buttonContainerEl.appendChild(answerButtonEl);
-        newMainBoxCenterEl.appendChild(buttonContainerEl);
+        answerButtonEl.setAttribute("style", "display: block;");
+        answerButtonEl.setAttribute("answer-number", Object.keys(testArr[id])[i]);
+        mainBoxCenterEl.appendChild(answerButtonEl);
     }
-
+    timerHandler();
+    id++
 }
 
-// first question
-var firstQuestionHandler = function(event) {
-    var targetEl = event.target;
-    if (targetEl.matches(".btn.start")) {
-        // remove elements from main-box top, center, and bottom and add new empty divs with styles
-        mainBoxTopEl.remove();
-        mainBoxCenterEl.remove();
-        mainBoxBottomEl.remove();
-        newMainBoxTopEl.className = "main-box top";
-        newMainBoxCenterEl.className = "main-box center";
-        newMainBoxCenterEl.className = "main-box bottom"
-        newMainBoxTopEl.setAttribute("style", "text-align: left;");
-        newMainBoxCenterEl.setAttribute("style", "text-align: left;")
+var clearDivContent = function() {
+    // remove elements from main-box top, center, and bottom and add new empty divs with styles
+    while (mainBoxTopEl.firstChild) {
+        mainBoxTopEl.removeChild(mainBoxTopEl.firstChild);
+    }
+    while (mainBoxCenterEl.firstChild) {
+        mainBoxCenterEl.removeChild(mainBoxCenterEl.firstChild);
+    }
+    while (mainBoxBottomEl.firstChild) {
+        mainBoxBottomEl.removeChild(mainBoxBottomEl.firstChild);
+    }
+}
 
-        // run button loop
-        buttonLoop();
-
-        // add divs to page
-        pageContentEl.appendChild(newMainBoxTopEl);
-        pageContentEl.appendChild(newMainBoxCenterEl);
-        pageContentEl.appendChild(newMainBoxBottomEl);
-
-        // run timerHandler
-        timerHandler();
+// timer
+var timerHandler = function() {
+    if (!id) {
+        var timerInterval = setInterval(function() {
+            timeLeft--;
+            timerEl.textContent = timeLeft;
+            if (timeLeft === 0) {
+                clearInterval(timerInterval);
+            }
+        }, 1000);
     }
 };
 
-// second question
-var secondQuestionHandler = function(event) {
+var answerHandler = function(event) {
     var targetEl = event.target;
-    if (targetEl.match(".btn.answer")) {
-
+    if (targetEl.matches(".btn.answer")) {
+        var answer = testArr[id - 1].a;
+        var answerAttr = parseInt(targetEl.getAttribute("answer-number"));
+        if (answer === answerAttr) {
+            timeLeft += 5;
+        }
+        else {
+            timeLeft -= 5;
+        }
     }
-}
+};
 
-pageContentEl.addEventListener("click", firstQuestionHandler);
+pageContentEl.addEventListener("click", answerHandler);
+pageContentEl.addEventListener("click", contentHandler);
